81e6d9af703870517f193baa14817e3e1ff357ee
xalmanfaris <xalmanfarisp@gmail.com>
2026-06-29 09:42:42 +0530

    Initial project upload

--- YuGo/obj/Debug/net10.0/YuGo.AssemblyInfo.cs
+++ YuGo/obj/Debug/net10.0/YuGo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YuGo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c1fd365a773b331d150bfa5bdff0a4902fc30cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("YuGo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YuGo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
